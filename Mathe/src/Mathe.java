bd537e94ef92bbcf34d6ee222bf5599c77dea03e
Dahup Buhambwe <andrejkolontai@gmail.com>
2017-01-06 16:36:30 +0100

    erster commit

---
+++ Mathe/src/Mathe.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Mathe {
+
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			while(true) {
+				int a = rnd.nextInt(10);
+				int b = rnd.nextInt(10-a);
+				System.out.print(a+"+"+b+"=");
+				int antwort = Integer.parseInt(in.readLine());
+				if (antwort == a+b){
+					System.out.println(":-)");
+				}else{
+					System.out.println("/-(");
+				}
+			}
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
